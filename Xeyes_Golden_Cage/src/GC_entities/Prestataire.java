4956da193edebc135e620d4cde89052792afd4b7
cyrineh <cyrinee.hassinee@gmail.com>
2014-02-21 15:20:07 +0100

    Projet initinal + ajouter_prestataire

---
+++ Xeyes_Golden_Cage/src/GC_entities/Prestataire.java
@@ -0,0 +1,135 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GC_entities;
+
+/**
+ *
+ * @author Cyrine
+ */
+public class Prestataire 
+{
+    private int id;
+    private String adresse_mail;
+    private String pseudo;
+    private String logo;
+    private String adresse;
+    private String service;
+    private String motDePasse;
+    private String tel;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the adresse_mail
+     */
+    public String getAdresse_mail() {
+        return adresse_mail;
+    }
+
+    /**
+     * @param adresse_mail the adresse_mail to set
+     */
+    public void setAdresse_mail(String adresse_mail) {
+        this.adresse_mail = adresse_mail;
+    }
+
+    /**
+     * @return the pseudo
+     */
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    /**
+     * @param pseudo the pseudo to set
+     */
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    /**
+     * @return the logo
+     */
+    public String getLogo() {
+        return logo;
+    }
+
+    /**
+     * @param logo the logo to set
+     */
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    /**
+     * @return the adresse
+     */
+    public String getAdresse() {
+        return adresse;
+    }
+
+    /**
+     * @param adresse the adresse to set
+     */
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    /**
+     * @return the service
+     */
+    public String getService() {
+        return service;
+    }
+
+    /**
+     * @param service the service to set
+     */
+    public void setService(String service) {
+        this.service = service;
+    }
+
+    /**
+     * @return the motDePasse
+     */
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    /**
+     * @param motDePasse the motDePasse to set
+     */
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    /**
+     * @return the tel
+     */
+    public String getTel() {
+        return tel;
+    }
+
+    /**
+     * @param tel the tel to set
+     */
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    
+    
+}
---
+++ Xeyes_Golden_Cage/src/GC_test/Xeyes_Golden_Cage.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GC_test;
+
+import GC_util.MyConnection;
+
+/**
+ *
+ * @author Cyrine
+ */
+public class Xeyes_Golden_Cage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MyConnection.getInstance();
+    }
+}
